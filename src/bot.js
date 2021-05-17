92fd8b5027ead1af3dfe51c2beec27f45a56f44d
akshay-vinod <akshayvinod468@gmail.com>
2021-05-17 17:48:00 +0530

    now bot is able to check slot availability of vaccine in your district

--- src/bot.js
+++ src/bot.js
@@ -1,34 +1,81 @@
 require("dotenv").config();
+const {fetchState,fetchDistricts,fetchSlots} = require("./state")
 
-//console.log(process.env.BOT_TOKEN);
 
 const { Client } = require("discord.js");
 const client = new Client();
 
 const PREFIX = "$";
+//to store states
+var stateData = []
+var districtData =[]
+var slotData = []
 
-client.on("ready", () => {
-  console.log(`${client.user.username}`);
 
+function search(nameKey, myArray){
+    for (var i=0; i < myArray.length; i++) {
+        if ((myArray[i].state_name ).toUpperCase()=== (nameKey).toUpperCase()) {
+            return myArray[i].state_id;
+        }
+    }
+}
+//var resultObject = search("string 1", array);
+
+client.on("ready", async() => {
+  const state = await fetchState();
+  stateData = state.states
 });
 
-client.on("message",(message) => {
+client.on("message",async(message) => {
   if(message.author.bot)
     return;
   if(message.content === "hello"){
     message.reply("Hello there")
   }
   if(message.content.startsWith(PREFIX)){
-    const [CMD_NAME,...arguments] = message.content
+    const [CMD_NAME,arguments] = message.content
     .trim()
     .substring(PREFIX.length)
-    .split(/\s+/);
+    .split(/\s(.+)?/,2);
     if(CMD_NAME === "start"){
+        stateData.map(items=>{
+            message.channel.send(items.state_name)
+        })
+      
       message.channel.send("Enter your state name in format '$state statename'")
-      message.channel.send("Kerala Tamil Nadu Karnataka Telangana Maharashtra Gujarat")
-    }else if(CMD_NAME === "state"){
+      
+    }
+    else if(CMD_NAME === "state"){
+      resultObject = search(arguments, stateData);
+      result = stateData.find(({state_name}) => state_name.toUpperCase() === arguments.toUpperCase());
+      if(!result){
+        message.channel.send(`Invalid state name.Please try again.`)
+        return
+      }
+      
+      const district = await fetchDistricts(result.state_id);
+      var districtsMessage = ""
+      districtData= district.districts
+      districtData.map(items=>{
+        districtsMessage+=" `"+items.district_name+"`"
+      })
+      message.channel.send(districtsMessage)
       message.channel.send("Enter your district name in format '$district districtname'")
-      message.channel.send("Trivandrum Thrissur Ernakulam Kozhikode Malapuram kasargod");
+    }
+    else if (CMD_NAME === "district"){
+        result = districtData.find(x => (x.district_name).toUpperCase() === arguments.toUpperCase());
+        if(!result){
+          message.channel.send("Invalid district name");
+          return;
+        }
+        const slot = await fetchSlots(result.district_id);
+        slotMessage = ""
+        slotData = slot.sessions
+        slotData.map((items)=>{
+            slotMessage+=" `"+items.name+" "+items.vaccine+" ("+items.available_capacity+") `";
+        })
+        message.channel.send(slotMessage)
+
     }
   }
 })
---
+++ src/state.js
@@ -0,0 +1,37 @@
+const fetch = require("node-fetch");
+headers = {
+  "User-Agent":
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+};
+const fetchState = async () => {
+  /*fetch(`https://cdn-api.co-vin.in/api/v2/admin/location/districts/16`)
+    .then((res) => console.log(res))
+    .then((json) => console.log(json));*/
+
+  /*response = requests.get(url, (headers = headers));*/
+  url = "https://cdn-api.co-vin.in/api/v2/admin/location/states";
+  return fetch(url, { method: "GET", headers: headers })
+    .then((res) => {
+      const resJson = res.json();
+      return resJson;
+    })
+    .catch((err) => console.log(err));
+};
+
+const fetchDistricts = async (state_id) => {
+  url = `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${state_id}`;
+  return fetch(url, { headers: headers }).then(async (res) => {
+    const resJson = await res.json();
+    return resJson;
+  });
+};
+
+const fetchSlots = async (district_id) => {
+  url = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${district_id}&date=31-03-2021`;
+  return fetch(url, { headers: headers }).then(async (res) => {
+    const resJson = await res.json();
+    return resJson;
+  });
+};
+
+module.exports = { fetchState, fetchDistricts, fetchSlots };
