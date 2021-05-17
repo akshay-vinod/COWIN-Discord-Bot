439e5b6b52c4e3dbd58249b5f592b4a927226397
akshay-vinod <akshayvinod468@gmail.com>
2021-05-17 18:55:18 +0530

    server location issue in heroku

--- src/state.js
+++ src/state.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 headers = {
   "User-Agent":
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0",
 };
 const fetchState = async () => {
   /*fetch(`https://cdn-api.co-vin.in/api/v2/admin/location/districts/16`)
@@ -10,7 +10,14 @@ const fetchState = async () => {
 
   /*response = requests.get(url, (headers = headers));*/
   url = "https://cdn-api.co-vin.in/api/v2/admin/location/states";
-  return fetch(url, { method: "GET", headers: headers })
+  return fetch(url, {
+    method: "GET",
+    headers: headers,
+    proxy: {
+      host: "13.235.248.19",
+      port: 3128,
+    },
+  })
     .then(async (res) => {
       const resJson = res.json();
       return resJson;
@@ -20,7 +27,13 @@ const fetchState = async () => {
 
 const fetchDistricts = async (state_id) => {
   url = `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${state_id}`;
-  return fetch(url, { headers: headers }).then(async (res) => {
+  return fetch(url, {
+    headers: headers,
+    proxy: {
+      host: "13.235.248.19",
+      port: 3128,
+    },
+  }).then(async (res) => {
     const resJson = await res.json();
     return resJson;
   });
@@ -28,7 +41,13 @@ const fetchDistricts = async (state_id) => {
 
 const fetchSlots = async (district_id) => {
   url = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${district_id}&date=31-03-2021`;
-  return fetch(url, { headers: headers }).then(async (res) => {
+  return fetch(url, {
+    headers: headers,
+    proxy: {
+      host: "13.235.248.19",
+      port: 3128,
+    },
+  }).then(async (res) => {
     const resJson = await res.json();
     return resJson;
   });
